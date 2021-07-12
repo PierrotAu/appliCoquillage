7a22ca9abee876642f0819d2f2bdefd53f30cd5f
pierre <pierre.auzeau@gmail.com>
2021-07-12 10:15:50 +0200

    ajout de la connexion firebase

--- appliCoquillage/src/app/app.module.ts
+++ appliCoquillage/src/app/app.module.ts
@@ -7,10 +7,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from "@angular/fire/storage";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyC08oU2z8dr1TFiQSGXJtozuSsG5FBbI5E",
+    authDomain: "fabrik-bb98f.firebaseapp.com",
+    projectId: "fabrik-bb98f",
+    storageBucket: "fabrik-bb98f.appspot.com",
+    messagingSenderId: "545313494223",
+}
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
